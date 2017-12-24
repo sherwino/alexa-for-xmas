d79815539c74487f69ef7307710d8440e23db15e
Sherwino <sherwination@gmail.com>
2017-12-24 03:56:44 -0500

    Intent changes

--- lambda/custom/presents.js
+++ lambda/custom/presents.js
@@ -7,10 +7,10 @@ module.exports = {
      'mom': 'Merry Christmas mom, you now have yourself an Echo Dot, the first step to turn the house into a smart home.',
      'mom\'s second': 'This present is going to require that you check your email, it is a little something from Groupon. Hopefully you like getting foot massages',
      'rosy': 'I think you got your self a tiny little box, that, well, should help you keep your phone on. Don\'t lose it!',
-     'rosy\'s second': 'ho ho ho, Apparently you get more than one preseent this year. Impressive. You might want to take a look at the poorly wrapped present with the lamas all over it. Remember that time that Mia ate your kindle? Well hopefully she won\'t eat this one',
+     'rosy\'s second': 'ho ho ho, Apparently you get more than one present this year. Impressive. You might want to take a look at the poorly wrapped present with the llamas all over it. Remember that time that Mia ate your kindle? Well hopefully she won\'t eat this one',
      'jerel': 'Since you use your playstation to watch Netflix, YouTube, and Hulu more than anything else, I thought it might be cool to get you a remote. But this remote should work with the TV too, so hopefully you could control everything with this one remote. Maybe',
-     'jerel\'s second': 'Your second gift is a little bit fancyer, you spend a bit of time on the bus, and the train these days so how about some good headphones. Ones that not only sound great but have a built in mic that you could use.',
-     'rosy\'s third': 'Well, well, well, you have yourself an appointment to make, how about a foot massage for you too. Best part about it, is that it is next to your job.'
+     'jerel\'s second': 'Your second gift is a little bit fancy er, you spend a bit of time on the bus, and on the train, these days so how about some good headphones. Ones that not only sound great but have a built in mic that you could use.',
+     'rosy\'s 3rd': 'Well, well, well, you have yourself an appointment to make, how about a foot massage for you too. Best part about it, is that it is next to your job.'
 
 
 
